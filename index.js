66a06a09bf80896a08b41dd7159855893ace2d42
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2023-10-06 10:37:57 +0300

    route handler for /auth/google/callback

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-require('dotenv').config();
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("dotenv").config();
 
 const app = express();
 
@@ -10,16 +10,19 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: "/auth/google/callback",
     },
-    (accessToken) => console.log(accessToken)
+    (accessToken, refreshToken, profile, done) =>
+      console.log(accessToken, refreshToken, profile, done)
   )
 );
 
 app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
