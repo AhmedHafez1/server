8f653ef37babf698ffacca3051fb491c6d4930a4
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-10-06 08:19:30 +0300

    OAuth initial setup

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
 
 const app = express();
 
-passport.use(new GoogleStrategy({}));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => console.log(accessToken)
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
