f85b36af1c638251e40869d173022dc8acccfc93
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-10-06 21:13:56 +0300

    Added routes and services

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-require("dotenv").config();
+const express = require('express');
+require('dotenv').config();
+require('./services/passport');
+const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) =>
-      console.log(accessToken, refreshToken, profile, done)
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+// Routes
+app.use('/auth', authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
