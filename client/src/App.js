83a272ff7e1dd8ec78c060322cc499e62530fb68
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-10-14 07:00:33 +0300

    header

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import Header from './components/Header';
 const Landing = () => <h1>Landing</h1>;
+const Dashboard = () => <h1>Dashboard</h1>;
+const NewSurvey = () => <h1>NewSurvey</h1>;
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route exact path="/" Component={Landing} />
+        <Route exact path="/surveys" Component={Dashboard} />
+        <Route path="/surveys/new" Component={NewSurvey} />
       </Routes>
     </div>
   );
---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          Surveys
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a>Login With Google </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
